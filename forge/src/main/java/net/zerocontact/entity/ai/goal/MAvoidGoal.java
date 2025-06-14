f8ddc21438ef03be1ef1c05c89714e752bd15ff7
TuDouNi92 <potato-craft@hotmail.com>
2025-06-14 11:19:40 +0800

    MOD: Optimize Hurt avoid goal

--- forge/src/main/java/net/zerocontact/entity/ai/goal/MAvoidGoal.java
+++ forge/src/main/java/net/zerocontact/entity/ai/goal/MAvoidGoal.java
@@ -1,21 +1,21 @@
 package net.zerocontact.entity.ai.goal;
 
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.phys.Vec3;
 import net.zerocontact.entity.ArmedRaider;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-
 public class MAvoidGoal extends Goal {
     private final ArmedRaider mob;
-    private final float distance;
+    private final int distance;
     @Nullable
-    private final Random random = new Random();
+    private final RandomSource random;
 
-    public MAvoidGoal(ArmedRaider mob, float distance) {
+    public MAvoidGoal(ArmedRaider mob, int distance) {
         this.mob = mob;
         this.distance = distance;
+        this.random = mob.getRandom();
     }
 
     @Override
@@ -25,8 +25,11 @@ public class MAvoidGoal extends Goal {
 
     @Override
     public void tick() {
-        Vec3 targetPos =mob.position().add(0,0,-1);
-        mob.getNavigation().moveTo(targetPos.x,targetPos.y,targetPos.z,1.0D);
+        if (random == null) return;
+        int targetX = (random.nextBoolean() ? 1 : -1) * random.nextInt(distance);
+        int targetZ = (random.nextBoolean() ? 1 : -1) * random.nextInt(distance);
+        Vec3 targetPos = mob.position().add(targetX, 0, targetZ);
+        if (!mob.getNavigation().isDone()) return;
+        mob.getNavigation().moveTo(targetPos.x, targetPos.y, targetPos.z, 1.2D);
     }
-
 }
