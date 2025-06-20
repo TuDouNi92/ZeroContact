72d0025cd4b8ba30cd6ef07cd2b0ca276459098f
TuDouNi92 <potato-craft@hotmail.com>
2025-06-20 18:34:31 +0800

    ADD: Ratnki 6b47 helmet class

---
+++ forge/src/main/java/net/zerocontact/item/helmet/Ratnik.java
@@ -0,0 +1,115 @@
+package net.zerocontact.item.helmet;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.zerocontact.api.DurabilityLossProvider;
+import net.zerocontact.api.EntityHurtProvider;
+import net.zerocontact.api.HelmetInfoProvider;
+import net.zerocontact.client.renderer.HelmetRender;
+import net.zerocontact.events.ProtectionLevelHelper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Ratnik extends ArmorItem implements HelmetInfoProvider, GeoItem, EntityHurtProvider, DurabilityLossProvider {
+    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private final Type type;
+    private final ArmorMaterial material;
+    private final int defense;
+    private static final int defaultDurability = 16;
+    private final int absorb;
+    public Ratnik(ArmorMaterial material, Type type, Properties properties, int absorb) {
+        super(material, type, properties.defaultDurability(defaultDurability));
+        this.type = type;
+        this.material = material;
+        this.defense = 3;
+        this.absorb = absorb;
+    }
+
+    @Override
+    public @NotNull Type getType() {
+        return type;
+    }
+
+    @Override
+    public @NotNull ArmorMaterial getMaterial() {
+        return material;
+    }
+
+    @Override
+    public int getDefense() {
+        return defense;
+    }
+
+    @Override
+    public int getDefaultDurability() {
+        return 0;
+    }
+
+    @Override
+    public int getAbsorb() {
+        return absorb;
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> modifierMultimap = HashMultimap.create();
+        stack.getOrCreateTag().putInt("absorb", getAbsorb());
+        return modifierMultimap;
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
+        Component tipsToAdd = Component.translatable(ProtectionLevelHelper.get(getAbsorb()).name()).withStyle(ChatFormatting.AQUA);
+        tooltipComponents.add(tipsToAdd);
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+    @Override
+    public void initializeClient(@NotNull Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+            private HelmetRender render;
+            @Override
+            public @NotNull HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+                if (this.render == null) {
+                    this.render = new HelmetRender();
+                }
+                this.render.prepForRender(livingEntity, itemStack, equipmentSlot, original);
+                return this.render;
+            }
+
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return new HelmetRender.HelmetItemRender();
+            }
+        });
+    }
+}
