d197712154fe4d9b31e5ffc7262f42b8fefae088
TuDouNi92 <potato-craft@hotmail.com>
2025-06-29 18:01:05 +0800

    MOD: Optimize data-driven and add a new uniform type

--- forge/src/main/java/net/zerocontact/item/armor/forge/GenerateArmorGeoImpl.java
+++ forge/src/main/java/net/zerocontact/item/armor/forge/GenerateArmorGeoImpl.java
@@ -10,7 +10,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.zerocontact.ZeroContact;
--- forge/src/main/java/net/zerocontact/item/helmet/GenerateHelmetGeoImpl.java
+++ forge/src/main/java/net/zerocontact/item/helmet/GenerateHelmetGeoImpl.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
