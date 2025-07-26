5430171cb557cfbdb88b927d53a1654eeec53688
TuDouNi92 <potato-craft@hotmail.com>
2025-07-26 11:03:58 +0800

    MOD: Move code

--- forge/src/main/java/net/zerocontact/events/ModForgeEventBus.java
+++ forge/src/main/java/net/zerocontact/events/ModForgeEventBus.java
@@ -1,6 +1,9 @@
 package net.zerocontact.events;
 
+import com.tacz.guns.api.event.common.EntityHurtByGunEvent;
 import com.tacz.guns.entity.EntityKineticBullet;
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.common.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -9,19 +12,40 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.zerocontact.client.interaction.BulletPassBy;
 import net.zerocontact.command.ToggleStaminaCommand;
+import net.zerocontact.network.ModMessages;
+import net.zerocontact.stamina.PlayerStamina;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModForgeEventBus {
     @SubscribeEvent
     public static void onEntity(EntityEvent event) {
         Entity entity = event.getEntity();
-        if( entity instanceof EntityKineticBullet){
-            BulletPassBy.playBulletPassBySound(entity,Minecraft.getInstance().player);
+        if (entity instanceof EntityKineticBullet) {
+            BulletPassBy.playBulletPassBySound(entity, Minecraft.getInstance().player);
         }
     }
+
     @SubscribeEvent
     public static void RegCommands(RegisterCommandsEvent event) {
         ToggleStaminaCommand.register(event.getDispatcher());
     }
 
+    public static void regEvents() {
+        ModMessages.register();
+        TickEvent.PLAYER_PRE.register(PlayerStamina::staminaTick);
+        dev.architectury.event.events.common.EntityEvent.LIVING_HURT.register((lv, source, amount) -> {
+                    PlateDamageEvent.DamagePlateRegister(lv, source, amount);
+                    if (PlateEntityHurtEvent.changeHurtAmountRicochet(lv, source, amount, EventUtil.idHitFromBack(lv, source))) {
+                        return EventResult.interruptFalse();
+                    }
+                    return EventResult.pass();
+                }
+        );
+    }
+
+    @SubscribeEvent
+    public static void entityHurtByGunEvent(EntityHurtByGunEvent event) {
+        PlateEntityHurtEvent.entityHurtByGunHeadShot(event);
+        PlateDamageEvent.DamageHelmet(event);
+    }
 }
--- forge/src/main/java/net/zerocontact/forge/ZeroContactForge.java
+++ forge/src/main/java/net/zerocontact/forge/ZeroContactForge.java
@@ -1,22 +1,12 @@
 package net.zerocontact.forge;
 
-import com.tacz.guns.api.event.common.EntityHurtByGunEvent;
-import dev.architectury.event.EventResult;
-import dev.architectury.event.events.common.EntityEvent;
-import dev.architectury.event.events.common.TickEvent;
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.zerocontact.ZeroContact;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.zerocontact.datagen.Predicate;
-import net.zerocontact.events.EntityDeathDogTagEvent;
-import net.zerocontact.events.EventUtil;
-import net.zerocontact.events.PlateDamageEvent;
-import net.zerocontact.events.PlateEntityHurtEvent;
+import net.zerocontact.events.*;
 import net.zerocontact.forge_registries.ModEntitiesReg;
-import net.zerocontact.network.ModMessages;
-import net.zerocontact.stamina.PlayerStamina;
 import software.bernie.geckolib.GeckoLib;
 
 @Mod(ZeroContact.MOD_ID)
@@ -26,30 +16,9 @@ public class ZeroContactForge {
         EventBuses.registerModEventBus(ZeroContact.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ZeroContact.init();
         GeckoLib.initialize();
-        EventRegister.regEvents();
+        ModForgeEventBus.regEvents();
         ModEntitiesReg.register();
         Predicate.predicateCurios();
         EntityDeathDogTagEvent.register();
     }
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
-    static class EventRegister {
-        private static void regEvents() {
-            ModMessages.register();
-            TickEvent.PLAYER_PRE.register(PlayerStamina::staminaTick);
-            EntityEvent.LIVING_HURT.register((lv, source, amount) -> {
-                        PlateDamageEvent.DamagePlateRegister(lv, source, amount);
-                        if (PlateEntityHurtEvent.changeHurtAmountRicochet(lv, source, amount, EventUtil.idHitFromBack(lv, source))) {
-                            return EventResult.interruptFalse();
-                        }
-                        return EventResult.pass();
-                    }
-            );
-        }
-        @SubscribeEvent
-        public static void entityHurtByGunEvent(EntityHurtByGunEvent event) {
-            PlateEntityHurtEvent.entityHurtByGunHeadShot(event);
-            PlateDamageEvent.DamageHelmet(event);
-        }
-    }
-
 }
