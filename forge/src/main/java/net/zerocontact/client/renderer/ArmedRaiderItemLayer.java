c10e4238b83eadb11f86ede19bb2f2007af495f5
TuDouNi92 <potato-craft@hotmail.com>
2025-07-30 15:06:20 +0800

    MOD: Update TACZ compability to 1.1.5 and above

--- forge/src/main/java/net/zerocontact/client/renderer/ArmedRaiderItemLayer.java
+++ forge/src/main/java/net/zerocontact/client/renderer/ArmedRaiderItemLayer.java
@@ -1,22 +1,14 @@
 package net.zerocontact.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import com.tacz.guns.api.item.gun.AbstractGunItem;
-import com.tacz.guns.client.renderer.item.GunItemRenderer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.minecraft.client.renderer.ItemInHandRenderer;
+import com.tacz.guns.client.renderer.item.GunItemRendererWrapper;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.extensions.common.IClientBlockExtensions;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.zerocontact.entity.ArmedRaider;
 import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoRenderer;
 import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
@@ -46,7 +38,7 @@ public class ArmedRaiderItemLayer extends BlockAndItemGeoLayer<ArmedRaider> {
             RenderUtils.prepMatrixForBone(poseStack,bone);
             poseStack.translate(-0.5,-0.5,0.375);
             poseStack.mulPose(Axis.XN.rotationDegrees(90));
-            GunItemRenderer blockEntityWithoutLevelRenderer = (GunItemRenderer) IClientItemExtensions.of(stack).getCustomRenderer();
+            GunItemRendererWrapper blockEntityWithoutLevelRenderer = (GunItemRendererWrapper) IClientItemExtensions.of(stack).getCustomRenderer();
             if(blockEntityWithoutLevelRenderer==null)return;
             blockEntityWithoutLevelRenderer.renderByItem(stack, ItemDisplayContext.THIRD_PERSON_RIGHT_HAND,poseStack,bufferSource,packedLight,packedOverlay);
             poseStack.popPose();
