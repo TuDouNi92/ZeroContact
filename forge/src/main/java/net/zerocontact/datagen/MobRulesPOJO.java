cc63295e92ed8191408e07122194be49876b36fb
TuDouNi92 <potato-craft@hotmail.com>
2026-09-06 19:35:28 +0800

    Fix(damage): Remove incorrect initializer

--- forge/src/main/java/net/zerocontact/datagen/MobRulesPOJO.java
+++ forge/src/main/java/net/zerocontact/datagen/MobRulesPOJO.java
@@ -15,9 +15,5 @@ public class MobRulesPOJO {
             @SerializedName("bodyshot_multiplier")
             float bodyshotMultiplier
     ) {
-        public Pattern {
-            headshotMultiplier = headshotMultiplier == 0.0f ? 1 : headshotMultiplier;
-            bodyshotMultiplier = bodyshotMultiplier == 0.0f ? 1 : bodyshotMultiplier;
-        }
     }
 }
